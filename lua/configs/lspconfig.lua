33494be5398d9ae4154ca4c71d634db1588a5fa1
Lyhua-tech <slyhour69@gmail.com>
2026-05-15 09:55:27 +0700

    fix typo tailwind in server

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,14 +1,12 @@
-local lspconfig = require "lspconfig"
 local nvlsp = require "nvchad.configs.lspconfig"
 
--- 1. Standard servers
-local servers = { "html", "cssls", "ts_ls", "tailwindcss:" }
-for _, lsp in ipairs(servers) do
-  lspconfig[lsp].setup {
+local servers = { "html", "cssls", "ts_ls", "tailwindcss" }
+
+for _, server in ipairs(servers) do
+  vim.lsp.config(server, {
     on_attach = nvlsp.on_attach,
     on_init = nvlsp.on_init,
     capabilities = nvlsp.capabilities,
-  }
+  })
+  vim.lsp.enable(server)
 end
--- 3. Vue (Volar) Setup
-local ok, mason_registry = pcall(require, "mason-registry")
